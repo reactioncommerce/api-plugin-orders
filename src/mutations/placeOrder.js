0a79c13c377a0d01e01685c341fee608bf2f1685
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2021-08-21 17:43:31 -0600

    add more logs

--- src/mutations/placeOrder.js
+++ src/mutations/placeOrder.js
@@ -250,6 +250,7 @@ export default async function placeOrder(context, input) {
   };
 
   const odooObject = await createOdooBilling(context, order);
+  console.log("odooObject", odooObject);
   if(odooObject){
     if(odooObject.order){
       order["idOdooBilling"] = odooObject.order.id;
